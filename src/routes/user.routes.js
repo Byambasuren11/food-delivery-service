7d191f243e76a4b2e486f45fa25b7eeb9efc0671
Byambasuren11 <byumbasurenbold@gmail.com>
2025-04-03 17:50:43 +0800

    this is test

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-// import { getUser } from "../controller/users/get-User.js";
+import { Users } from "../models/users.model.js";
 import { deleteUser } from "../controller/users/delete-User.js";
 import { validateEmailAndPassword } from "../middleware/authentication/validateEmailAndPssword.js";
 import { loginUser } from "../controller/users/create-User.js";
@@ -8,8 +8,22 @@ import { updateUser } from "../controller/users/put-User.js";
 
 export const userRouter = express.Router();
 
+export const getUser = async (req, res) => {
+  try {
+    const users = await Users.find({});
+    res.status(200).json({
+      success: true,
+      data: users,
+      message: `Successfully`,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: true, message: "Internal Error" });
+  }
+};
+
 userRouter.post("/", validateEmailAndPassword, validateEmail, loginUser);
 // userRouter.post("/", validateEmailAndPassword, loginUser);
-// userRouter.get("/", getUser);
+userRouter.get("/", getUser);
 userRouter.delete("/", deleteUser);
 userRouter.put("/", updateUser);
