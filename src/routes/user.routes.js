f409210b183de79494618055801ee9a2aeba02cc
Byambasuren11 <byumbasurenbold@gmail.com>
2025-04-03 17:47:36 +0800

    commeted

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUser } from "../controller/users/get-User.js";
+// import { getUser } from "../controller/users/get-User.js";
 import { deleteUser } from "../controller/users/delete-User.js";
 import { validateEmailAndPassword } from "../middleware/authentication/validateEmailAndPssword.js";
 import { loginUser } from "../controller/users/create-User.js";
@@ -10,6 +10,6 @@ export const userRouter = express.Router();
 
 userRouter.post("/", validateEmailAndPassword, validateEmail, loginUser);
 // userRouter.post("/", validateEmailAndPassword, loginUser);
-userRouter.get("/", getUser);
+// userRouter.get("/", getUser);
 userRouter.delete("/", deleteUser);
 userRouter.put("/", updateUser);
